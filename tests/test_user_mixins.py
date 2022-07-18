e1d2e77d20e0b0e440af3edd69a2132decb23899
Rob Dailey <rob@wargam.es>
2022-07-17 22:27:26 -0400

    Linting

--- tests/test_user_mixins.py
+++ tests/test_user_mixins.py
@@ -8,7 +8,10 @@ class TestUserMixin:
     async def test_basic(self, app, mixin_user_class, mock_users):
         mixin_guard = sanic_praetorian.Praetorian(app, mixin_user_class)
 
-        the_dude = await mock_users(username="the_dude", password="abides", guard_name=mixin_guard, class_name=mixin_user_class)
+        the_dude = await mock_users(username="the_dude",
+                                    password="abides",
+                                    guard_name=mixin_guard,
+                                    class_name=mixin_user_class)
 
         assert await mixin_guard.authenticate("the_dude", "abides") == the_dude
         with pytest.raises(sanic_praetorian.exceptions.AuthenticationError):
