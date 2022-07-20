9b088757944cfe8d6c2b5d67ee58fcd748dfa60e
Rob Dailey <rob@wargam.es>
2022-07-20 14:03:24 -0400

    Tested and working

--- example/custom_claims.py
+++ example/custom_claims.py
@@ -1,153 +1,204 @@
-import flask
-import tempfile
-import flask_sqlalchemy
-import flask_praetorian
-import flask_cors
+from tortoise.contrib.sanic import register_tortoise
+from tortoise.models import Model
+from tortoise import fields
+from tortoise.exceptions import DoesNotExist
 
-db = flask_sqlalchemy.SQLAlchemy()
-guard = flask_praetorian.Praetorian()
-cors = flask_cors.CORS()
+from sanic import Sanic, json
+
+import sanic_praetorian
+from sanic_praetorian import Praetorian
+from sanic_mailing import Mail
 
 
-# A generic user model that might be used by an app powered by flask-praetorian
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, unique=True)
-    password = db.Column(db.Text)
-    roles = db.Column(db.Text)
-    is_active = db.Column(db.Boolean, default=True, server_default='true')
-    firstname = db.Column(db.Text)
-    surname = db.Column(db.Text)
-    nickname = db.Column(db.Text)
+_guard = Praetorian()
+_mail = Mail()
+
+
+# A generic user model that might be used by an app powered by sanic-praetorian
+class User(Model):
+    """
+    Provides a basic user model for use in the tests
+    """
+
+    class Meta:
+        table = "User"
+
+    id = fields.IntField(pk=True)
+    username = fields.CharField(unique=True, max_length=255)
+    password = fields.CharField(max_length=255)
+    email = fields.CharField(max_length=255, unique=True)
+    roles = fields.CharField(max_length=255, default='')
+    is_active = fields.BooleanField(default=True)
+    nickname = fields.CharField(allow_none=True, max_length=255, default='')
+
+    def __str__(self):
+        return f"User {self.id}: {self.username}"
 
     @property
     def rolenames(self):
+        """
+        *Required Attribute or Property*
+
+        sanic-praetorian requires that the user class has a :py:meth:``rolenames``
+        instance attribute or property that provides a list of strings that
+        describe the roles attached to the user instance.
+
+        This can be a seperate table (probably sane), so long as this attribute
+        or property properly returns the associated values for the user as a
+        list of strings.
+        """
         try:
-            return self.roles.split(',')
+            return self.roles.split(",")
         except Exception:
             return []
 
     @classmethod
-    def lookup(cls, username):
-        return cls.query.filter_by(username=username).one_or_none()
+    async def lookup(cls, username=None, email=None):
+        """
+        *Required Method*
+
+        sanic-praetorian requires that the user class implements a :py:meth:``lookup()``
+        class method that takes a single ``username`` or ``email`` argument and
+        returns a user instance if there is one that matches or ``None`` if
+        there is not.
+        """
+        try:
+            if username:
+                return await cls.filter(username=username).get()
+            elif email:
+                return await cls.filter(email=email).get()
+            else:
+                return None
+        except DoesNotExist:
+            return None
 
     @classmethod
-    def identify(cls, id):
-        return cls.query.get(id)
+    async def identify(cls, id):
+        """
+        *Required Attribute or Property*
+
+        sanic-praetorian requires that the user class implements an :py:meth:``identify()``
+        class method that takes a single ``id`` argument and returns user instance if
+        there is one that matches or ``None`` if there is not.
+        """
+        try:
+            return await cls.filter(id=id).get()
+        except DoesNotExist:
+            return None
 
     @property
     def identity(self):
+        """
+        *Required Attribute or Property*
+
+        sanic-praetorian requires that the user class has an :py:meth:``identity``
+        instance attribute or property that provides the unique id of the user
+        instance
+        """
         return self.id
 
-    def is_valid(self):
-        return self.is_active
 
-
-# Initialize flask app for the example
-app = flask.Flask(__name__)
-app.debug = True
-app.config['SECRET_KEY'] = 'top secret'
-app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
-app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
-
-# Initialize the flask-praetorian instance for the app
-guard.init_app(app, User)
-
-# Initialize a local database for the example
-local_database = tempfile.NamedTemporaryFile(prefix='local', suffix='.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(local_database)
-db.init_app(app)
-
-# Initializes CORS so that the api_tool can talk to the example app
-cors.init_app(app)
-
-# Add users for the example
-with app.app_context():
-    db.create_all()
-    db.session.add(User(
-        username='TheDude',
-        password=guard.hash_password('abides'),
-        firstname='Jeffrey',
-        nickname='The Dude',
-        surname='Lebowski',
-    ))
-    db.session.add(User(
-        username='Walter',
-        password=guard.hash_password('calmerthanyouare'),
-        roles='admin',
-        firstname='Walter',
-        surname='Sobchak',
-    ))
-    db.session.add(User(
-        username='Donnie',
-        password=guard.hash_password('iamthewalrus'),
-        roles='operator',
-        firstname='Theodore',
-        nickname='Donnie',
-        surname='Kerabatsos',
-    ))
-    db.session.add(User(
-        username='Maude',
-        password=guard.hash_password('andthorough'),
-        roles='operator,admin',
-        firstname='Maude',
-        nickname='Maudie',
-        surname='Lebowski',
-    ))
-    db.session.commit()
-
-
-# Set up some routes for the example
-
-@app.route('/login', methods=['POST'])
-def login():
+def create_app(db_path=None):
     """
-    Logs a user in by parsing a POST request containing user credentials and
-    issuing a JWT token.
-    .. example::
-       $ curl http://localhost:5000/login -X POST \
-         -d '{"username":"Walter","password":"calmerthanyouare"}'
+    Initializes the sanic app for the test suite. Also prepares a set of routes
+    to use in testing with varying levels of protections
     """
-    req = flask.request.get_json(force=True)
-    username = req.pop('username', None)
-    password = req.pop('password', None)
+    sanic_app = Sanic('sanic-testing')
+    # In order to process more requests after initializing the app,
+    # we have to set degug to false so that it will not check to see if there
+    # has already been a request before a setup function
+    sanic_app.config.FALLBACK_ERROR_FORMAT = "json"
 
-    user = guard.authenticate(username, password)
-    ret = {'access_token': guard.encode_jwt_token(
-        user,
-        firstname=user.firstname,
-        nickname=user.nickname,
-        surname=user.surname,
-    )}
+    # sanic-praetorian config
+    sanic_app.config.SECRET_KEY = "top secret"
+    sanic_app.config["JWT_ACCESS_LIFESPAN"] = {"hours": 24}
+    sanic_app.config["JWT_REFRESH_LIFESPAN"] = {"days": 30}
 
-    return (flask.jsonify(ret), 200)
+    # sanic-mailing config
+    sanic_app.config.MAIL_SERVER = 'localhost:25'
+    sanic_app.config.MAIL_USERNAME = ''
+    sanic_app.config.MAIL_PASSWORD = ''
+    sanic_app.config.MAIL_FROM = 'fake@fake.com'
+    sanic_app.config.JWT_PLACES = ['header', 'cookie']
 
+    _guard.init_app(sanic_app, User)
+    sanic_app.ctx.mail = _mail
 
-@app.route('/protected')
-@flask_praetorian.auth_required
-def protected():
-    """
-    A protected endpoint. The auth_required decorator will require a header
-    containing a valid JWT
-    .. example::
-       $ curl http://localhost:5000/protected -X GET \
-         -H "Authorization: Bearer <your_token>"
-    """
-    custom_claims = flask_praetorian.current_custom_claims()
-    firstname = custom_claims.pop('firstname', None)
-    nickname = custom_claims.pop('nickname', None)
-    surname = custom_claims.pop('surname', None)
-
-    if nickname is None:
-        user_string = "{} {}".format(firstname, surname)
-    else:
-        user_string = "{} '{}' {}".format(firstname, nickname, surname)
-
-    return flask.jsonify(
-        message="protected endpoint (allowed user {u})".format(u=user_string),
+    register_tortoise(
+        sanic_app,
+        db_url='sqlite://:memory:',
+        modules={"models": ['__main__']},
+        generate_schemas=True,
     )
+
+    # Add users for the example
+    @sanic_app.listener('before_server_start')
+    async def populate_db(sanic):
+        await User.create(username="the_dude",
+                          nickname="The Dude",
+                          email="the_dude@praetorian.test.io",
+                          password=_guard.hash_password("abides"),)
+
+        await User.create(username="walter",
+                          nickname="Walter",
+                          email="walter@praetorian.test.io",
+                          password=_guard.hash_password("calmerthanyouare"),
+                          roles="admin",)
+
+        await User.create(username="donnie",
+                          nickname="Donnie",
+                          email="donnie@praetorian.test.io",
+                          password=_guard.hash_password("iamthewalrus"),
+                          roles="operator",)
+
+        await User.create(username="maude",
+                          nickname="Maude",
+                          password=_guard.hash_password("andthorough"),
+                          email="maude@praetorian.test.io",
+                          roles="operator,admin",)
+
+    # Set up some routes for the example
+    @sanic_app.route("/login", methods=["POST"])
+    async def login(request):
+        """
+        Logs a user in by parsing a POST request containing user credentials and
+        issuing a JWT token.
+        .. example::
+           $ curl http://localhost:8000/login -X POST \
+             -d '{"username":"Walter","password":"calmerthanyouare"}'
+        """
+        req = request.json
+        username = req.get("username", None)
+        password = req.get("password", None)
+        user = await _guard.authenticate(username, password)
+        ret = {'access_token': await _guard.encode_jwt_token(
+            user,
+            nickname=user.nickname,
+        )}
+
+
+        return json(ret, status=200)
+
+    @sanic_app.route("/protected")
+    @sanic_praetorian.auth_required
+    async def protected(request):
+        """
+        A protected endpoint. The auth_required decorator will require a header
+        containing a valid JWT
+        .. example::
+           $ curl http://localhost:8000/protected -X GET \
+             -H "Authorization: Bearer <your_token>"
+        """
+        user = await sanic_praetorian.current_user()
+        custom_claims = sanic_praetorian.current_custom_claims()
+        nickname = custom_claims.pop('nickname', None)
+
+        return json({"message": f"protected endpoint (allowed user {user.username} aka {nickname})"})
+
+    return sanic_app
 
 
 # Run the example
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5030)
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="127.0.0.1", port=8000, workers=1, debug=True)
