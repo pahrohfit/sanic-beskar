f5a8ae4d9b7d361d12c0dcde14c8676d86cc2233
Rob Dailey <rob@wargam.es>
2022-07-20 09:54:18 -0400

    Working and tested

--- example/basic_mongo.py
+++ example/basic_mongo.py
@@ -1,182 +1,235 @@
-import flask
-import flask_praetorian
-import flask_cors
-import mongoengine
-from mongoengine import Document, fields, DoesNotExist, signals
-from bson.json_util import loads, dumps
+from sanic import Sanic, json
 
-db = mongoengine
-db.connect("praetorian", host="localhost", port=27017)
+from bson.objectid import ObjectId
 
-guard = flask_praetorian.Praetorian()
-cors = flask_cors.CORS()
+import sanic_praetorian
+from sanic_praetorian import Praetorian
+from sanic_mailing import Mail
+from sanic.log import logger
 
+from umongo import Document, EmbeddedDocument, fields, validate, post_load
+from umongo.exceptions import NotCreatedError
 
-class RoleException(Exception):
-    pass
-
-
-allowed_roles = ["admin", "operator"]
+from umongo.frameworks.motor_asyncio import MotorAsyncIOInstance
+#from umongo.frameworks import MongoMockInstance
+from mongomock_motor import AsyncMongoMockClient
 
 
-class User(Document):
+_guard = Praetorian()
+_mail = Mail()
+
+
+def create_app(db_path=None):
     """
-    This is a small sample of a User class that persists to MongoDB.
-
-    The following docker-compose.yml snippet can be used for testing.
-    Please do not use in production.
-
-    version: "3.2"
-    services:
-    mongo:
-        image: mongo:latest
-        container_name: "mongo"
-        restart: always
-        ports:
-        - 27017:27017
-
+    Initializes the sanic app for the test suite. Also prepares a set of routes
+    to use in testing with varying levels of protections
     """
+    sanic_app = Sanic('sanic-testing')
+    sanic_app.config['PYTESTING'] = True
+    # In order to process more requests after initializing the app,
+    # we have to set degug to false so that it will not check to see if there
+    # has already been a request before a setup function
+    sanic_app.config.FALLBACK_ERROR_FORMAT = "json"
 
-    username = fields.StringField(required=True, unique=True)
-    password = fields.StringField(required=True)
-    roles = fields.StringField()
-    is_active = fields.BooleanField(default=True)
+    # sanic-praetorian config
+    sanic_app.config.SECRET_KEY = "top secret"
+    sanic_app.config["JWT_ACCESS_LIFESPAN"] = {"hours": 24}
+    sanic_app.config["JWT_REFRESH_LIFESPAN"] = {"days": 30}
 
-    @classmethod
-    def lookup(cls, username):
-        try:
-            return User.objects(username=username).get()
-        except DoesNotExist:
-            return None
+    # sanic-mailing config
+    sanic_app.config.MAIL_SERVER = 'localhost:25'
+    sanic_app.config.MAIL_USERNAME = ''
+    sanic_app.config.MAIL_PASSWORD = ''
+    sanic_app.config.MAIL_FROM = 'fake@fake.com'
+    sanic_app.config.JWT_PLACES = ['header', 'cookie']
 
-    @classmethod
-    def identify(cls, id):
-        try:
-            return User.objects(id=loads(id)).get()
-        except DoesNotExist:
-            return None
+    sanic_app.ctx.mail = _mail
 
-    @property
-    def rolenames(self):
-        try:
-            roles = self.roles.split(",")
-            if set(roles).issubset(set(allowed_roles)):
-                return roles
-            else:
-                raise RoleException
-        except RoleException:
-            return []
+    db = AsyncMongoMockClient()['test']
+    instance = MotorAsyncIOInstance(db)
+    instance.set_db(db)
 
-    @property
-    def identity(self):
-        return dumps(self.id)
+    # A generic user model that might be used by an app powered by sanic-praetorian
+    @instance.register
+    class User(Document):
+        """
+        Provides a basic user model for use in the tests
+        """
+    
+        class Meta:
+            table = "User"
+    
+        id = fields.ObjectIdField()
+        username = fields.StringField(allow_none=False, unique=True, validate=[validate.Length(max=255)])
+        password = fields.StringField(allow_none=False, validate=[validate.Length(max=255)])
+        email = fields.StringField(unique=True, allow_none=False, validate=[validate.Length(max=128)])
+        roles = fields.StringField(load_default='')
+        is_active = fields.BooleanField(load_default=True)
+    
+        def __str__(self):
+            return f"User {self.id}: {self.username}"
+    
+        @property
+        def rolenames(self):
+            """
+            *Required Attribute or Property*
+    
+            sanic-praetorian requires that the user class has a :py:meth:``rolenames``
+            instance attribute or property that provides a list of strings that
+            describe the roles attached to the user instance.
+    
+            This can be a seperate table (probably sane), so long as this attribute
+            or property properly returns the associated values for the user as a
+            list of strings.
+            """
+            try:
+                return self.roles.split(",")
+            except Exception:
+                return []
+    
+        @classmethod
+        async def lookup(cls, username=None, email=None):
+            """
+            *Required Method*
+    
+            sanic-praetorian requires that the user class implements a :py:meth:``lookup()``
+            class method that takes a single ``username`` or ``email`` argument and
+            returns a user instance if there is one that matches or ``None`` if
+            there is not.
+            """
+            try:
+                if username:
+                    return await User.find_one({'username': username})
+                elif email:
+                    return await User.find_one({'email': email})
+                else:
+                    return None
+            except NotCreatedError:
+                return None
+    
+        @classmethod
+        async def identify(cls, id):
+            """
+            *Required Attribute or Property*
+    
+            sanic-praetorian requires that the user class implements an :py:meth:``identify()``
+            class method that takes a single ``id`` argument and returns user instance if
+            there is one that matches or ``None`` if there is not.
+            """
+            try:
+                return await cls.find_one({'id': ObjectId(id)})
+            except NotCreatedError:
+                logger.critical(f'Could not find by id: {id}')
+                return None
+    
+        @property
+        def identity(self):
+            """
+            *Required Attribute or Property*
+    
+            sanic-praetorian requires that the user class has an :py:meth:``identity``
+            instance attribute or property that provides the unique id of the user
+            instance
+            
+            Mongo's `id`, by default, is an ObjectId, which cannot be serialized by
+            default -- so return as as a string value instead!
+            """
+            return str(self.id)
 
-    def is_valid(self):
-        return self.is_active
+    _guard.init_app(sanic_app, User)
+
+    # Add users for the example
+    @sanic_app.listener('before_server_start')
+    async def setup_example_db(sanic, loop):
+        logger.critical(f'User: {User}')
+        await User.ensure_indexes()
+
+        await User(username="the_dude",
+                   email="the_dude@praetorian.test.io",
+                   password=_guard.hash_password("abides"),
+        ).commit()
+
+        await User(username="Walter",
+                   email="walter@praetorian.test.io",
+                   password=_guard.hash_password("calmerthanyouare"),
+                   roles="admin",
+        ).commit()
+
+        await User(username="Donnie",
+                   email="donnie@praetorian.test.io",
+                   password=_guard.hash_password("iamthewalrus"),
+                   roles="operator",
+        ).commit()
+
+        await User(username="Maude",
+                   password=_guard.hash_password("andthorough"),
+                   email="maude@praetorian.test.io",
+                   roles="operator,admin",
+        ).commit()
 
 
-def update_modified(sender, document):
-    try:
-        print(document.save())
-    except Exception:
-        pass
+    # Set up some routes for the example
+    @sanic_app.route("/login", methods=["POST"])
+    async def login(request):
+        """
+        Logs a user in by parsing a POST request containing user credentials and
+        issuing a JWT token.
+        .. example::
+           $ curl http://localhost:8000/login -X POST \
+             -d '{"username":"Walter","password":"calmerthanyouare"}'
+        """
+        req = request.json
+        username = req.get("username", None)
+        password = req.get("password", None)
+        user = await _guard.authenticate(username, password)
 
+        ret = {"access_token": await _guard.encode_jwt_token(user)}
+        return json(ret, status=200)
 
-signals.post_init.connect(update_modified)
+    @sanic_app.route("/protected")
+    @sanic_praetorian.auth_required
+    async def protected(request):
+        """
+        A protected endpoint. The auth_required decorator will require a header
+        containing a valid JWT
+        .. example::
+           $ curl http://localhost:8000/protected -X GET \
+             -H "Authorization: Bearer <your_token>"
+        """
+        user = await sanic_praetorian.current_user()
+        return json({"message": f"protected endpoint (allowed user {user.username})"})
 
-# Initialize flask app for the example
-app = flask.Flask(__name__)
-app.debug = True
-app.config["SECRET_KEY"] = "top secret"
-app.config["JWT_ACCESS_LIFESPAN"] = {"hours": 24}
-app.config["JWT_REFRESH_LIFESPAN"] = {"days": 30}
+    @sanic_app.route("/protected_admin_required")
+    @sanic_praetorian.roles_required("admin")
+    async def protected_admin_required(request):
+        """
+        A protected endpoint that requires a role. The roles_required decorator
+        will require that the supplied JWT includes the required roles
+        .. example::
+           $ curl http://localhost:8000/protected_admin_required -X GET \
+              -H "Authorization: Bearer <your_token>"
+        """
+        user = await sanic_praetorian.current_user()
+        return json({"message": f"protected_admin_required endpoint (allowed user {user.username})"})
 
-# Initialize the flask-praetorian instance for the app
-guard.init_app(app, User)
+    @sanic_app.route("/protected_operator_accepted")
+    @sanic_praetorian.roles_accepted("operator", "admin")
+    async def protected_operator_accepted(request):
+        """
+        A protected endpoint that accepts any of the listed roles. The
+        roles_accepted decorator will require that the supplied JWT includes at
+        least one of the accepted roles
+        .. example::
+           $ curl http://localhost/protected_operator_accepted -X GET \
+             -H "Authorization: Bearer <your_token>"
+        """
+        user = await sanic_praetorian.current_user()
+        return json({"message": f"protected_operator_accepted endpoint (allowed usr {user.username}"})
 
-# Initializes CORS so that the api_tool can talk to the example app
-cors.init_app(app)
-
-User(username="TheDude", password=guard.hash_password("abides"), roles="")
-User(username="Walter", password=guard.hash_password("calmerthanyouare"), roles="admin")
-User(username="Donnie", password=guard.hash_password("iamthewalrus"), roles="admin")
-User(
-    username="Maude",
-    password=guard.hash_password("andthorough"),
-    roles="operator,admin",
-)
-
-# Set up some routes for the example
-@app.route("/login", methods=["POST"])
-def login():
-    """
-    Logs a user in by parsing a POST request containing user credentials and
-    issuing a JWT token.
-    .. example::
-       $ curl http://localhost:5000/login -X POST \
-         -d '{"username":"Walter","password":"calmerthanyouare"}'
-    """
-    req = flask.request.get_json(force=True)
-    username = req.get("username", None)
-    password = req.get("password", None)
-    user = guard.authenticate(username, password)
-    ret = {"access_token": guard.encode_jwt_token(user)}
-    return (flask.jsonify(ret), 200)
-
-
-@app.route("/protected")
-@flask_praetorian.auth_required
-def protected():
-    """
-    A protected endpoint. The auth_required decorator will require a header
-    containing a valid JWT
-    .. example::
-       $ curl http://localhost:5000/protected -X GET \
-         -H "Authorization: Bearer <your_token>"
-    """
-    return flask.jsonify(
-        message="protected endpoint (allowed user {})".format(
-            flask_praetorian.current_user().username,
-        )
-    )
-
-
-@app.route("/protected_admin_required")
-@flask_praetorian.roles_required("admin")
-def protected_admin_required():
-    """
-    A protected endpoint that requires a role. The roles_required decorator
-    will require that the supplied JWT includes the required roles
-    .. example::
-       $ curl http://localhost:5000/protected_admin_required -X GET \
-          -H "Authorization: Bearer <your_token>"
-    """
-    return flask.jsonify(
-        message="protected_admin_required endpoint (allowed user {})".format(
-            flask_praetorian.current_user().username,
-        )
-    )
-
-
-@app.route("/protected_operator_accepted")
-@flask_praetorian.roles_accepted("operator", "admin")
-def protected_operator_accepted():
-    """
-    A protected endpoint that accepts any of the listed roles. The
-    roles_accepted decorator will require that the supplied JWT includes at
-    least one of the accepted roles
-    .. example::
-       $ curl http://localhost/protected_operator_accepted -X GET \
-         -H "Authorization: Bearer <your_token>"
-    """
-    return flask.jsonify(
-        message="protected_operator_accepted endpoint (allowed usr {})".format(
-            flask_praetorian.current_user().username,
-        )
-    )
+    return sanic_app
 
 
 # Run the example
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app = create_app()
+    app.run(host="127.0.0.1", port=8000, workers=1, debug=True)
