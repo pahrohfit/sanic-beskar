20f0c1dad45687e1eada5bb2d67a6afd2317a28e
rob dailey <rob@suspected.org>
2023-03-21 14:18:38 -0400

    Linting

--- sanic_beskar/orm/__init__.py
+++ sanic_beskar/orm/__init__.py
@@ -4,20 +4,20 @@
 try:
     from .tortoise_user_mixins import TortoiseUserMixin
     TortoiseUserMixin  # to shut up pyflakes
-except (ModuleNotFoundError, ImportError) as e:
+except (ModuleNotFoundError, ImportError):
     # this is ok, this is optional
     pass
 
 try:
     from .umongo_user_mixins import UmongoUserMixin
     UmongoUserMixin  # to shut up pyflakes
-except (ModuleNotFoundError, ImportError) as e:
+except (ModuleNotFoundError, ImportError):
     # this is ok, this is optional
     pass
 
 try:
     from .beanie_user_mixins import BeanieUserMixin
     BeanieUserMixin # to shut up pyflakes
-except (ModuleNotFoundError, ImportError) as e:
+except (ModuleNotFoundError, ImportError):
     # this is ok, this is optional
     pass
