7ae1165a84a89f7a161eb8c974d8955b9b5b6504
Rob Dailey <rob@wargam.es>
2023-03-23 17:18:01 -0400

    Coverage cleanup

--- sanic_beskar/orm/__init__.py
+++ sanic_beskar/orm/__init__.py
@@ -1,23 +1,23 @@
 # flake8: noqa
 # I know this is ugly, will clean it up later
 
-try:
+try: # pragma: no cover
     from .tortoise_user_mixins import TortoiseUserMixin
     TortoiseUserMixin  # to shut up pyflakes
-except (ModuleNotFoundError, ImportError):
+except (ModuleNotFoundError, ImportError): # pragma: no cover
     # this is ok, this is optional
     pass
 
-try:
+try: # pragma: no cover
     from .umongo_user_mixins import UmongoUserMixin
     UmongoUserMixin  # to shut up pyflakes
-except (ModuleNotFoundError, ImportError):
+except (ModuleNotFoundError, ImportError): # pragma: no cover
     # this is ok, this is optional
     pass
 
-try:
+try: # pragma: no cover
     from .beanie_user_mixins import BeanieUserMixin
     BeanieUserMixin # to shut up pyflakes
-except (ModuleNotFoundError, ImportError):
+except (ModuleNotFoundError, ImportError): # pragma: no cover
     # this is ok, this is optional
     pass
